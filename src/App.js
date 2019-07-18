13bf6cab3a3615e783795ad28706408d1f85c46f
Death <mathrox431@yahoo.com>
2019-07-18 15:06:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+
+      <div class="header">
+        <h1>Chania</h1>
+      </div>
+
+      <div class="row">
+        <div class="col-3 menu">
+          <ul>
+            <li>The Flight</li>
+            <li>The City</li>
+            <li>The Island</li>
+            <li>The Food</li>
+          </ul>
+        </div>
+
+        <div class="col-6">
+          <h1>The City</h1>
+          <p>
+            Chania is the capital of the Chania region on the island of Crete.
+            The city can be divided in two parts, the old town and the modern
+            city.
+          </p>
+        </div>
+
+        <div class="col-3 right">
+          <div class="aside">
+            <h2>What?</h2>
+            <p>Chania is a city on the island of Crete.</p>
+            <h2>Where?</h2>
+            <p>Crete is a Greek island in the Mediterranean Sea.</p>
+            <h2>How?</h2>
+            <p>You can reach Chania airport from all over Europe.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Resize the browser window to see how the content respond to the
+          resizing.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
